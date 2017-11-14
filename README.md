0dd96d80473376a9a8056de9bfe4e0a04cbb49b9
Philipp Hauer <info@philipphauer.de>
2017-11-14 18:25:30 +0100

    initial commit
    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# bulk-url-checker
+# Bulk-URL-Checker
+
+Checks a given bulk of URL from a csv file for:
+
+- Status code
+- Redirect target
+- Valid HTML (a local [Nu-Validator](https://github.com/validator/validator) service is used. So you don't have to upload your site for validation. Just run the validator against localhost)
+- Some Metadata in the HTML head (language, title, canonical URL)
+- Search for PHP include error messages
+
+This way, you can do refactorings on your website and be sure, that nothing critical broke.
+
+## Usage
+
+You csv file should look like this:
+
+```csv
+full_url,expected_status_code,expected_redirect_target
+https://www.domain.de/path/,200
+https://www.domain.de/oldPath,301,https://www.domain.de/newPath/
+http://www.domain.de/path/,301,https://www.domain.de/path/
+https://www.domain.de/path/?queryparam,200
+```
+
+```bash
+# live:
+./bulk-url-checker.py --csv_file="urls-live.csv" --nu_validator_url="https://validator.w3.org/nu/"
+
+# live URLs but local nu-validator
+docker-compose up # start the nu-validator locally
+./bulk-url-checker.py --csv_file="urls-live.csv" --nu_validator_url="http://localhost:8888/"
+
+# on localhost (assuming that you have a local nu-validator up and running):
+./bulk-url-checker.py --csv_file="urls-localhost.csv" --nu_validator_url="http://localhost:8888/"
+```
+
+## Install Python 3.6 and Deps
+
+See [here](https://askubuntu.com/questions/865554/how-do-i-install-python-3-6-using-apt-get). For Ubuntu 16.04:
+
+```bash
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6 python3.6-dev
+
+# many ubuntu components (like gnome-terminal) are relying on python 3.5.
+# So you can't change the symlink python3 to point to 3.6
+# sudo rm /usr/bin/python3
+# sudo ln -s /usr/bin/python3.6 /usr/bin/python3
+
+# re-install requirements with pip
+sudo pip3 install -r requirements.txt
+```
+
+## Misc
+
+I received a list of the used URLs of my web site from Google Analytics. It provides an csv-export functionality. To remove all data from the csv except the URLs use the following command:
+
+```bash
+awk -F "\"*,\"*" '{print $1}' file.csv
+```
---
+++ bulk-url-checker.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3.6
+import csv
+import time
+from multiprocessing.pool import ThreadPool
+from typing import List, NamedTuple
+
+import click
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class UrlConfig(NamedTuple):
+    full_url: str
+    expected_status_code: int
+    expected_redirect_target: str
+
+
+@click.command()
+@click.option('--csv_file', required=True, help='The csv file containing the urls to check')
+@click.option('--nu_validator_url', default=None, help='If provided, each page will be validated using the given validator url. Defaults to None (no validation). Example values: "https://validator.w3.org/nu/" or "http://localhost:8888/"')
+@click.option('--thread_amount', type=click.INT, default=10, help='How many process are started to execute the check in parallel.')
+def check(csv_file: str, nu_validator_url: str, thread_amount: int):
+    """Checks a given amount of URLs (e.g. status codes, redirects, title, valid HTML etc.)"""
+    start_time = time.time()
+    url_configs = read_url_configs(csv_file)
+    url_config_chunks = partition(url_configs, thread_amount)
+
+    pool = ThreadPool(processes=thread_amount)
+    async_results = [pool.apply_async(func=check_urls, args=[chunk, nu_validator_url]) for chunk in url_config_chunks]
+    all_results = [result.get() for result in async_results]
+    errors = flatmap(all_results)
+    print(f"--- {time.time() - start_time} seconds ---")
+
+    if len(errors) > 0:
+        click.echo(f"==== !! {len(errors)} ERROR(S) OCCURRED !! ===== ")
+        for error in errors:
+            click.echo(error)
+    else:
+        click.echo("Everything went fine!")
+
+
+def read_url_configs(file_name: str) -> List[UrlConfig]:
+    with open(file_name, newline='\n', encoding="utf-8") as urlFile:
+        reader = csv.DictReader(urlFile, delimiter=',', quotechar='|')
+        return [map_to_url_config(row) for row in reader]
+
+
+def map_to_url_config(row):
+    return UrlConfig(full_url=row["full_url"],
+                     expected_status_code=int(row["expected_status_code"]),
+                     expected_redirect_target=row["expected_redirect_target"])
+
+
+def check_urls(url_config_chunk: List[UrlConfig], nu_validator_url: str) -> List[str]:
+    errors = []
+    for url_config in url_config_chunk:
+        errors += check_url(url_config, nu_validator_url)
+    return errors
+
+
+def check_url(url_config: UrlConfig, nu_validator_url: str) -> List[str]:
+    url = url_config.full_url
+    expected_status_code = url_config.expected_status_code
+    expected_redirect_target = url_config.expected_redirect_target
+    print(f"Checking {url_config}...")
+    is_local_url = "localhost" in url
+    response = requests.get(url, allow_redirects=False, verify=(not is_local_url))
+    if response.status_code == expected_status_code:
+        if expected_redirect_target:
+            location = response.headers['Location']
+            if location == expected_redirect_target:
+                return []
+            else:
+                return [f"URL {url} doesn't redirect to {expected_redirect_target}. Instead: {location}"]
+        else:
+            errors = []
+            errors += check_include_error(response.text, url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            errors += check_lang_definition(soup, url)
+            errors += check_canonical_link(soup, url)
+            errors += check_metadata(soup, url)
+            if nu_validator_url:
+                errors += validate_html(url, nu_validator_url)
+            return errors
+    else:
+        return [f"URL {url} doesn't respond with {expected_status_code}. Instead: {response.status_code}"]
+
+
+def validate_html(url: str, nu_validator_url: str) -> List[str]:
+    response = requests.get(url=nu_validator_url, params={'doc': url, 'out': 'json'})
+    messages = response.json()['messages']
+    if len(messages) != 0:
+        return [f"URL {url} has {len(messages)} validation errors. See {response.url.replace('out=json', 'out=html')}"]
+    return []
+
+
+def check_metadata(soup: BeautifulSoup, url) -> List[str]:
+    if soup.title is None or len(soup.title.text.strip()) == 0:
+        return [f"URL {url} doesn't have a title"]
+    return []
+
+
+def check_include_error(html_string: str, url: str) -> List[str]:
+    if "failed to open stream" in html_string:
+        return [f"URL {url} probably contains an include error."]
+    return []
+
+
+def check_canonical_link(soup: BeautifulSoup, request_url) -> List[str]:
+    canonical_link = soup.head.find('link', {'rel': 'canonical'})
+    if not canonical_link:
+        return [f"URL {request_url} doesn't contain canonical link."]
+    request_url_without_query_params = request_url[:request_url.index('?')] if '?' in request_url else request_url
+    canonical_href = canonical_link.attrs['href']
+    if canonical_href.endswith('/') and not request_url_without_query_params.endswith('/'): # "www.localhost/" vs "www.localhost"
+        return []
+    if canonical_link.attrs['href'] != request_url_without_query_params:
+        return [f"URL {request_url} contains a canonical url with a query parameter!"]
+    return []
+
+
+def check_lang_definition(soup: BeautifulSoup, url: str):
+    html_tag = soup.find('html')
+    if 'lang' not in html_tag.attrs or 'de' != html_tag.attrs['lang']:
+        return [f"URL {url} doesn't contain language 'de' in html tag."]
+    return []
+
+
+def partition(list, chunk_size):
+    division = len(list) / chunk_size
+    return [list[round(division * i):round(division * (i + 1))] for i in range(chunk_size)]
+
+
+def flatmap(list: List[str]):
+    return [item for sublist in list for item in sublist]
+
+
+if __name__ == '__main__':
+    check()
